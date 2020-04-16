41fdd14d40921631600ccfa05ece6fb001167d2f
Jan Kammerath <jankammerath@googlemail.com>
2020-04-16 11:20:10 +0200

    added condition names sample program

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cobc -x SRC/HELLOWORLD.COB -o BIN/HELLOWORLD
 cobc -x -free -frelax-syntax SRC/ADDAMT.COB -o BIN/ADDAMT
+cobc -x -free -frelax-syntax SRC/CONDITIONNAMES.COB -o BIN/CONDITIONNAMES
